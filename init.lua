b78f5e1a3bf6dc59ab229eb7400ed4034429b032
Zeioth <Zeioth@hotmail.com>
2023-05-25 01:38:19 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,9 @@
+-- open markman
+local cmd = vim.api.nvim_create_user_command
 local M = {}
 
--- TODO: do this stuff
--- antother thing
--- TODO: another TODO
-
 M.todo = function()
-  print("ALL OK")
+  cmd("MarkmapOpen", function()
+  	os.execute("markmap " .. vim.api.nvim_get_current_buf(bufnr))
+  end, { desc = "cd current file's directory" })
 end
-
-return M
