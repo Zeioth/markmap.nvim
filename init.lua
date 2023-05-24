ba75fc6dc0fa36e38912846966cbf2162e01d333
Zeioth <zeioth@hotmail.com>
2023-05-25 01:46:01 +0200

    x

--- init.lua
+++
@@ -1,6 +0,0 @@
--- open markman
-local cmd = vim.api.nvim_create_user_command
-
-cmd("MarkmapOpen", function()
-	os.execute("markmap " .. vim.api.nvim_get_current_buf(bufnr))
-end, { desc = "cd current file's directory" })
