834976cc869518f06a62d350df2e7e5bc0794275
Zeioth <Zeioth@hotmail.com>
2023-05-25 01:03:44 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@
 local cmd = vim.api.nvim_create_user_command
 
 cmd("MarkmapOpen", function()
-	os.execute("markmap " .. vim.api.nvim_get_current_buf(bufnr))
+	os.execute("markmap " .. "markmap " .. vim.fn.expand("%:p"))
 end, { desc = "cd current file's directory" })
