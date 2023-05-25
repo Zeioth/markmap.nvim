3a00d8c996fef2a1f5f67ecb86766954db1ba4a7
Zeioth <zeioth@hotmail.com>
2023-05-25 16:01:56 +0200

    Added: Async calls for watch

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -4,43 +4,70 @@ local cmd = vim.api.nvim_create_user_command
 local M = {}
 
 M.setup = function(ctx)
+	-- Setup options
+	local html_output = ctx.html_output
+	local hide_toolbar = ctx.hide_toolbar
 
-  -- Setup options
-  html_output = ctx.html_output
-  hide_toolbar = ctx.hide_toolbar
+	-- bool conditions
+	if html_output == nil then
+		html_output = "/tmp/markmap.html" -- by defaullt create the html file here
+	end
 
-  -- bool conditions
-  if (html_output == nil) then
-    html_output = "/tmp/markmap.html" -- by defaullt create the html file here
-  end
+	if hide_toolbar == nil then hide_toolbar = "" end
 
-  if (hide_toolbar == nil) then
-    hide_toolbar = ""
-  end
+	if hide_toolbar == true then hide_toolbar = " --no-toolbar" end
 
-  if (hide_toolbar == true) then
-    hide_toolbar = " --no-toolbar"
-  end
+	-- Setup autocmds
+	local handle = nil -- async runner we use for watch/watchstop
+	cmd(
+		"MarkmapOpen",
+		function()
+			os.execute(
+				"markmap "
+				.. vim.fn.expand "%:p"
+				.. " -o "
+				.. html_output
+				.. hide_toolbar
+			)
+		end,
+		{ desc = "Show a mental map of the current file" }
+	)
 
+	cmd(
+		"MarkmapSave",
+		function()
+			os.execute(
+				"markmap "
+				.. vim.fn.expand "%:p"
+				.. " -o "
+				.. html_output
+				.. " --no-open"
+			)
+		end,
+		{ desc = "Save the HTML file without opening the mindmap" }
+	)
 
-  -- Setup autocmds
-  cmd(
-  "MarkmapOpen", function()
-	  os.execute("markmap " .. vim.fn.expand("%:p") .. " -o " .. html_output .. hide_toolbar)
-	end, { desc = "Show a mental map of the current file" })
+	cmd(
+		"MarkmapWatch",
+		function()
+			-- If there was a server already running, stop it so they don't stack.
+			if handle ~= nil then handle.close() end
 
-  cmd(
-    "MarkmapSave", function()
-	    os.execute("markmap " .. vim.fn.expand("%:p") .. " -o " .. html_output .. " --no-open")
-	  end, { desc = "Save the HTML file without opening the mindmap" })
-  end
+			-- Now we can run
+			local execute = "markmap "
+					.. vim.fn.expand "%:p"
+					.. " -o "
+					.. html_output
+					.. hide_toolbar
+					.. " --watch"
+			handle = uv.spawn(execute, {}) -- run async
+		end,
+		{ desc = "Show a mental map of the current file and watch for changes" }
+	)
 
-  cmd(
-    "MarkmapWatch", function()
-      local cmd = "markmap " .. vim.fn.expand("%:p") .. " -o " .. html_output .. hide_toolbar .. " --watch"
-	    local handle = uv.spawn(cmd) -- run async
-	  end, { desc = "Show a mental map of the current file and watch for changes" }
-	  )
+	cmd("MarkmapWatchStop", function()
+		if handle ~= nil then handle.close() end
+	end, { desc = "Stop the Markmap server after using watch" })
 
-
-return M
+	return M
+end
