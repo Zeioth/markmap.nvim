a60e947f25b4a2ff7aff5849fe799cfa1681fa1e
Zeioth <zeioth@hotmail.com>
2023-05-26 00:58:30 +0200

    All works fine asyncronously now. But let's add a grace period option.

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -7,6 +7,7 @@ M.setup = function(ctx)
   -- Setup options
   local html_output = ctx.html_output
   local hide_toolbar = ctx.hide_toolbar
+  local grace_period = ctx.hide_toolbar
 
   -- bool conditions
   if html_output == nil then
@@ -28,44 +29,34 @@ M.setup = function(ctx)
   end
   if hide_toolbar then table.insert(arguments, hide_toolbar) end
 
-  -- Set job
+  -- Set a common job for all commands.
+  -- This prevents more than one job running at the same time.
   local job
 
   -- Setup autocmds
   cmd("MarkmapOpen", function()
-    -- Set extra arguments
     table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
-
-    -- If a job already exists, kill it before running another one
     if job then job.kill() end
-
-    -- Run the job
     job = uv.spawn(comando, { detached = true }, nil)
   end, { desc = "Show a mental map of the current file" })
 
   cmd("MarkmapSave", function()
-    -- Set extra arguments
     table.insert(arguments, "--no-open")         -- specific to this command
     table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
-
-    -- If a job already exists, kill it before running another one
     if job then job.kill() end
-
-    -- Run the job
     job = uv.spawn(comando, { detached = true }, nil)
   end, { desc = "Save the HTML file without opening the mindmap" })
 end
 
 cmd("MarkmapWatch", function()
-  -- Set extra arguments
   table.insert(arguments, "--watch")           -- spetific to this command
   table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
-
-  -- If a job already exists, kill it before running another one
   if job then job.kill() end
-
-  -- Run the job
   job = uv.spawn(comando, { detached = true }, nil)
 end, { desc = "Show a mental map of the current file and watch for changes" })
+
+cmd("MarkmapWatchStop", function()
+  if job then job.kill() end
+end, { desc = "Manually stops markmap watch" })
 
 return M
