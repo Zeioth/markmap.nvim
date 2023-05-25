7ac7f36fd9f3060be3a33bb462c4d596316db476
Zeioth <zeioth@hotmail.com>
2023-05-26 00:54:34 +0200

    Is visibility setted correctly?

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -5,8 +5,8 @@ local M = {}
 
 M.setup = function(ctx)
   -- Setup options
-  html_output = ctx.html_output
-  hide_toolbar = ctx.hide_toolbar
+  local html_output = ctx.html_output
+  local hide_toolbar = ctx.hide_toolbar
 
   -- bool conditions
   if html_output == nil then
@@ -19,29 +19,32 @@ M.setup = function(ctx)
     hide_toolbar = nil
   end
 
-  -- Global job
+  -- Set arguments
+  local arguments = {}
+  table.insert(arguments, "markmap")
+  if html_output ~= "" then -- if html_output is "", don't pass the parameter
+    table.insert(arguments, "-o")
+    table.insert(arguments, html_output)
+  end
+  if hide_toolbar then table.insert(arguments, hide_toolbar) end
+
+  -- Set job
   local job
 
   -- Setup autocmds
-  cmd(
-    "MarkmapOpen",
-    function()
-      os.execute(
-        "markmap " .. html_output .. hide_toolbar .. vim.fn.expand "%:p"
-      )
-    end,
-    { desc = "Show a mental map of the current file" }
-  )
+  cmd("MarkmapOpen", function()
+    -- Set extra arguments
+    table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
+
+    -- If a job already exists, kill it before running another one
+    if job then job.kill() end
+
+    -- Run the job
+    job = uv.spawn(comando, { detached = true }, nil)
+  end, { desc = "Show a mental map of the current file" })
 
   cmd("MarkmapSave", function()
-    -- Set arguments
-    local arguments = {}
-    table.insert "markmap"
-    if html_output ~= "" then -- if html_output is "", don't pass the parameter
-      table.insert(arguments, "-o")
-      table.insert(arguments, html_output)
-    end
-    if hide_toolbar then table.insert(arguments, hide_toolbar) end
+    -- Set extra arguments
     table.insert(arguments, "--no-open")         -- specific to this command
     table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
 
@@ -54,14 +57,7 @@ M.setup = function(ctx)
 end
 
 cmd("MarkmapWatch", function()
-  -- Set arguments
-  local arguments = {}
-  table.insert "markmap"
-  if html_output ~= "" then -- if html_output is "", don't pass the parameter
-    table.insert(arguments, "-o")
-    table.insert(arguments, html_output)
-  end
-  if hide_toolbar then table.insert(arguments, hide_toolbar) end
+  -- Set extra arguments
   table.insert(arguments, "--watch")           -- spetific to this command
   table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
 
