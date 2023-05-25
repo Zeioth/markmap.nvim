2d358c593cf1cbef8e8567392b0d7f75e8ee76e8
Zeioth <zeioth@hotmail.com>
2023-05-26 01:31:34 +0200

    Revert further

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -9,7 +9,7 @@ M.setup = function(ctx)
   hide_toolbar = ctx.hide_toolbar
 
   -- bool conditions
-  if html_output == nil then
+  if html_output == nil or html_output == "" then
     html_output = "/tmp/markmap.html" -- by defaullt create the html file here
   end
 
@@ -33,43 +33,32 @@ M.setup = function(ctx)
     { desc = "Show a mental map of the current file" }
   )
 
-  cmd("MarkmapSave", function()
-    -- Set arguments
-    local arguments = {}
-    table.insert "markmap"
-    if html_output ~= "" then -- if html_output is "", don't pass the parameter
-      table.insert(arguments, "-o")
-      table.insert(arguments, html_output)
-    end
-    if hide_toolbar then table.insert(arguments, hide_toolbar) end
-    table.insert(arguments, "--no-open")         -- specific to this command
-    table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
-
-    -- If a job already exists, kill it before running another one
-    if job then job.kill() end
-
-    -- Run the job
-    job = uv.spawn(comando, { detached = true }, nil)
-  end, { desc = "Save the HTML file without opening the mindmap" })
+  cmd(
+    "MarkmapSave",
+    function()
+      os.execute(
+        "markmap " .. html_output .. " --no-open " .. vim.fn.expand "%:p"
+      )
+    end,
+    { desc = "Save the HTML file without opening the mindmap" }
+  )
 end
 
 cmd("MarkmapWatch", function()
   -- Set arguments
   local arguments = {}
   table.insert "markmap"
-  if html_output ~= "" then -- if html_output is "", don't pass the parameter
-    table.insert(arguments, "-o")
-    table.insert(arguments, html_output)
-  end
+  table.insert(arguments, "-o")
+  table.insert(arguments, html_output)
   if hide_toolbar then table.insert(arguments, hide_toolbar) end
-  table.insert(arguments, "--watch")           -- spetific to this command
+  table.insert(arguments, "--watch")
   table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
 
   -- If a job already exists, kill it before running another one
   if job then job.kill() end
 
   -- Run the job
-  job = uv.spawn(comando, { detached = true }, nil)
+  local job = uv.spawn(comando, { detached = true }, nil)
 end, { desc = "Show a mental map of the current file and watch for changes" })
 
 return M
