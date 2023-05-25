518d499871daa1dfe56170621d70a4c09d5c6bbf
Zeioth <zeioth@hotmail.com>
2023-05-26 00:24:36 +0200

    Let's hardcode the output

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -48,7 +48,7 @@ cmd("MarkmapWatch", function()
   -- Set arguments
   local arguments = {}
   table.insert(arguments, "-o")
-  table.insert(arguments, html_output)
+  table.insert(arguments, "/tmp/markmap.html")
 
   --if hide_toolbar then table.insert(arguments, hide_toolbar) end
   table.insert(arguments, "--watch")
