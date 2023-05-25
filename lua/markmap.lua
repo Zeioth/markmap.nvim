bee82eea1c50e3ef24a1b78b36a1171a66aa1e54
Zeioth <zeioth@hotmail.com>
2023-05-25 22:52:55 +0200

    Let's try if these are the correct arguments for detach

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -3,45 +3,61 @@ local uv = vim.loop
 local cmd = vim.api.nvim_create_user_command
 local M = {}
 
-
 M.setup = function(ctx)
-
   -- Setup options
   html_output = ctx.html_output
   hide_toolbar = ctx.hide_toolbar
 
   -- bool conditions
-  if (html_output == nil) then
+  if html_output == nil then
     html_output = "/tmp/markmap.html" -- by defaullt create the html file here
   end
 
-  if (hide_toolbar == nil) then
-    hide_toolbar = ""
-  end
+  if hide_toolbar == nil then hide_toolbar = "" end
 
-  if (hide_toolbar == true) then
-    hide_toolbar = " --no-toolbar"
-  end
-
+  if hide_toolbar == true then hide_toolbar = " --no-toolbar" end
 
   -- Setup autocmds
   cmd(
-  "MarkmapOpen", function()
-	  os.execute("markmap " .. vim.fn.expand("%:p") .. " -o " .. html_output .. hide_toolbar)
-	end, { desc = "Show a mental map of the current file" })
+    "MarkmapOpen",
+    function()
+      os.execute(
+        "markmap "
+        .. vim.fn.expand "%:p"
+        .. " -o "
+        .. html_output
+        .. hide_toolbar
+      )
+    end,
+    { desc = "Show a mental map of the current file" }
+  )
 
   cmd(
-    "MarkmapSave", function()
-	    os.execute("markmap " .. vim.fn.expand("%:p") .. " -o " .. html_output .. " --no-open")
-	  end, { desc = "Save the HTML file without opening the mindmap" })
-  end
+    "MarkmapSave",
+    function()
+      os.execute(
+        "markmap "
+        .. vim.fn.expand "%:p"
+        .. " -o "
+        .. html_output
+        .. " --no-open"
+      )
+    end,
+    { desc = "Save the HTML file without opening the mindmap" }
+  )
+end
 
-  cmd(
-    "MarkmapWatch", function()
-      local run = "markmap " .. vim.fn.expand("%:p") .. " -o " .. html_output .. hide_toolbar .. " --watch"
-	    local handle = uv.spawn(run) -- run async
-	  end, { desc = "Show a mental map of the current file and watch for changes" }
-	  )
-
+cmd("MarkmapWatch", function()
+  local run_command = "markmap "
+      .. vim.fn.expand "%:p"
+      .. " -o "
+      .. html_output
+      .. hide_toolbar
+      .. " --watch"
+  handle = uv.spawn(run_command, {
+    stdio = { nil, nil, nil },
+    detached = true,
+  }, function(_, _, _) print "Comando ejecutado de forma asíncrona" end)
+end, { desc = "Show a mental map of the current file and watch for changes" })
 
 return M
