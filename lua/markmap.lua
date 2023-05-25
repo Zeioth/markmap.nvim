3bec24aa0cb0ffcf76af87fa7330c4b48d4e7848
Zeioth <zeioth@hotmail.com>
2023-05-26 01:27:36 +0200

    Revert further

--- lua/markmap.lua
+++ lua/markmap.lua
@@ -5,9 +5,8 @@ local M = {}
 
 M.setup = function(ctx)
   -- Setup options
-  local html_output = ctx.html_output
-  local hide_toolbar = ctx.hide_toolbar
-  local grace_period = ctx.hide_toolbar
+  html_output = ctx.html_output
+  hide_toolbar = ctx.hide_toolbar
 
   -- bool conditions
   if html_output == nil then
@@ -20,43 +19,57 @@ M.setup = function(ctx)
     hide_toolbar = nil
   end
 
-  -- Set arguments
-  local arguments = {}
-  table.insert(arguments, "markmap")
-  if html_output ~= "" then -- if html_output is "", don't pass the parameter
-    table.insert(arguments, "-o")
-    table.insert(arguments, html_output)
-  end
-  if hide_toolbar then table.insert(arguments, hide_toolbar) end
-
-  -- Set a common job for all commands.
-  -- This prevents more than one job running at the same time.
+  -- Global job
   local job
 
   -- Setup autocmds
-  cmd("MarkmapOpen", function()
-    table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
-    if job then job.kill() end
-    job = uv.spawn(comando, { detached = true }, nil)
-  end, { desc = "Show a mental map of the current file" })
+  cmd(
+    "MarkmapOpen",
+    function()
+      os.execute(
+        "markmap " .. html_output .. hide_toolbar .. vim.fn.expand "%:p"
+      )
+    end,
+    { desc = "Show a mental map of the current file" }
+  )
 
   cmd("MarkmapSave", function()
+    -- Set arguments
+    local arguments = {}
+    table.insert "markmap"
+    if html_output ~= "" then -- if html_output is "", don't pass the parameter
+      table.insert(arguments, "-o")
+      table.insert(arguments, html_output)
+    end
+    if hide_toolbar then table.insert(arguments, hide_toolbar) end
     table.insert(arguments, "--no-open")         -- specific to this command
     table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
+
+    -- If a job already exists, kill it before running another one
     if job then job.kill() end
+
+    -- Run the job
     job = uv.spawn(comando, { detached = true }, nil)
   end, { desc = "Save the HTML file without opening the mindmap" })
 end
 
 cmd("MarkmapWatch", function()
+  -- Set arguments
+  local arguments = {}
+  table.insert "markmap"
+  if html_output ~= "" then -- if html_output is "", don't pass the parameter
+    table.insert(arguments, "-o")
+    table.insert(arguments, html_output)
+  end
+  if hide_toolbar then table.insert(arguments, hide_toolbar) end
   table.insert(arguments, "--watch")           -- spetific to this command
   table.insert(arguments, vim.fn.expand "%:p") -- current buffer path
+
+  -- If a job already exists, kill it before running another one
   if job then job.kill() end
+
+  -- Run the job
   job = uv.spawn(comando, { detached = true }, nil)
 end, { desc = "Show a mental map of the current file and watch for changes" })
-
-cmd("MarkmapWatchStop", function()
-  if job then job.kill() end
-end, { desc = "Manually stops markmap watch" })
 
 return M
