2fc8c4f238b9f72b72dfdd9c7cfddcdd4aa18e5d
Zeioth <zeioth@hotmail.com>
2024-03-26 03:43:31 +0100

    format + both modes of jobstart have been tested.

--- lua/markmap/utils.lua
+++ lua/markmap/utils.lua
@@ -9,7 +9,7 @@ local is_windows = uv.os_uname().sysname == "Windows_NT"
 ---On unix, run it as string so it spawn a shell,
 ---so ENV is available, which is mandatory on termux.
 ---@param cmd string command to run.
----@param arguments table arguments to pass.
+---@param arguments table arguments to pass to the cmd.
 ---@return number job pid of the job, so we can stop it later.
 M.jobstart = function(cmd, arguments)
   if is_windows then
@@ -19,19 +19,19 @@ M.jobstart = function(cmd, arguments)
   end
 end
 
-  ---We re-set the arguments we pass to markmap every time we run it to ensure
-  ---they are always clean and correct, as every command get different ones.
-  ---@return table arguments
-  M.reset_arguments = function()
-    local config = vim.g.markmap_config
+---We re-set the arguments we pass to markmap every time we run it to ensure
+---they are always clean and correct, as every command get different ones.
+---@return table arguments
+M.reset_arguments = function()
+  local config = vim.g.markmap_config
 
-    local arguments = {}
-    if config.html_output ~= "" then -- if html_output is "", don't pass the parameter
-      table.insert(arguments, "-o")
-      table.insert(arguments, config.html_output)
-    end
-    if config.hide_toolbar then table.insert(arguments, config.hide_toolbar) end
-    return arguments
+  local arguments = {}
+  if config.html_output ~= "" then   -- if html_output is "", don't pass the parameter
+    table.insert(arguments, "-o")
+    table.insert(arguments, config.html_output)
   end
+  if config.hide_toolbar then table.insert(arguments, config.hide_toolbar) end
+  return arguments
+end
 
 return M
