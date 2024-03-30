2ece9162ab8a26f942ddfdcc085988ce48f8ab14
marcobuess <42165023+MarcoBuess@users.noreply.github.com>
2024-03-30 01:25:53 +0100

    feat: close markmap when watched buffer is closed
    This was a late one for me. Let me know how you like it, or if you can
    spot major flaws right away. cheers and happy easter. :)

--- lua/markmap/init.lua
+++ lua/markmap/init.lua
@@ -1,4 +1,6 @@
 -- This plugin is a wrapper for markmap-cli
+local api = vim.api
+local uv = vim.uv or vim.loop
 local utils = require("markmap.utils")
 local jobstart = utils.jobstart
 local jobstop = vim.fn.jobstop
@@ -27,48 +29,40 @@ M.setup = function(opts)
   cmd("MarkmapSave", function()
     config = vim.g.markmap_config
     arguments = utils.reset_arguments()
-    local path = '"' .. vim.fn.expand("%:p") .. '"' -- current buffer path
-    table.insert(arguments, "--no-open")            -- specific to this command
-    table.insert(arguments, path)
-    if job ~= nil then jobstop(job) end             -- kill jobs
+    table.insert(arguments, "--no-open") -- specific to this command
+    table.insert(arguments, vim.fn.expand("%:p")) -- current buffer path
+    if job ~= nil then jobstop(job) end -- kill jobs
     job = jobstart(config.markmap_cmd, arguments)
   end, { desc = "Save the HTML file without opening the mindmap" })
 
-  cmd("MarkmapWatch", function()
-    config = vim.g.markmap_config
-    arguments = utils.reset_arguments()
-    local path = '"' .. vim.fn.expand("%:p") .. '"' -- current buffer path
-    table.insert(arguments, "--watch")              -- spetific to this command
-    table.insert(arguments, path)
-    if job ~= nil then jobstop(job) end             -- kill jobs
-    job = jobstart(config.markmap_cmd, arguments)
-  end, { desc = "Show a mental map of the current file and watch for changes" })
+  cmd(
+    "MarkmapWatch",
+    function()
+      config = vim.g.markmap_config
+      arguments = utils.reset_arguments()
+      table.insert(arguments, "--watch") -- spetific to this command
+      table.insert(arguments, vim.fn.expand("%:p")) -- current buffer path
+      if job ~= nil then jobstop(job) end -- kill jobs
+      job = jobstart(config.markmap_cmd, arguments)
+
+      -- Register buffer local autocmd to kill job when buffer closes
+      local kill_on_close = augroup("markmap_kill_on_close", { clear = true })
+      autocmd("BufDelete", {
+        buffer = 0,
+        desc = "Kill markmap when watched buffer is closed",
+        group = kill_on_close,
+        callback = function()
+          jobstop(job)
+          api.nvim_clear_autocmds({ group = kill_on_close })
+        end,
+      })
+    end,
+    { desc = "Show a mental map of the current file and watch for changes" }
+  )
 
   cmd("MarkmapWatchStop", function()
-    if job ~= nil then jobstop(job) end             -- kill jobs
+    if job ~= nil then jobstop(job) end -- kill jobs
   end, { desc = "Manually stops markmap watch" })
-
-  -- Autocmds -----------------------------------------------------------------
-  -- Kill jobs after a grace period
-  local last_execution = vim.uv.now() -- timer for grace period
-  autocmd("CursorHold", {
-    desc = "Kill all markmap jobs after a grace period",
-    group = augroup("markmap_kill_after_grace_period", { clear = true }),
-    callback = function()
-      -- If grace_periodd is disabled, remove the autocmd and return
-      if config.grace_period == 0 then
-        vim.cmd "autocmd! markmap_kill_after_grace_period"
-        return
-      end
-
-      -- Otherwise, use grace_period
-      local current_time = vim.uv.now()
-      if current_time - last_execution >= config.grace_period then -- if grace period exceeded
-        if job ~= nil then jobstop(job) end                        -- pkill jobs
-        last_execution = current_time                              -- update time
-      end
-    end,
-  })
 end
 
 return M
