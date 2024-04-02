4013ab3b1355bd617ff888c2f37897dbcdcd1ff7
marcobuess <42165023+MarcoBuess@users.noreply.github.com>
2024-04-02 14:09:57 +0200

    feat(init): added tracking of watched buffers

--- lua/markmap/init.lua
+++ lua/markmap/init.lua
@@ -15,6 +15,7 @@ M.setup = function(opts)
   local config = vim.g.markmap_config
   local job = nil
   local arguments = {}
+  local lookup_table = {}
 
   -- Setup commands -----------------------------------------------------------
   cmd("MarkmapOpen", function()
@@ -38,30 +39,65 @@ M.setup = function(opts)
   cmd(
     "MarkmapWatch",
     function()
+      local watch_buffer = vim.api.nvim_get_current_buf()
       config = vim.g.markmap_config
       arguments = utils.reset_arguments()
       table.insert(arguments, "--watch") -- spetific to this command
       table.insert(arguments, vim.fn.expand("%:p")) -- current buffer path
-      if job ~= nil then jobstop(job) end -- kill jobs
-      job = jobstart(config.markmap_cmd, arguments)
 
-      -- Register buffer local autocmd to kill job when buffer closes
-      local kill_on_close = augroup("markmap_kill_on_close", { clear = true })
-      autocmd("BufDelete", {
-        buffer = 0,
-        desc = "Kill markmap when watched buffer is closed",
-        group = kill_on_close,
-        callback = function()
-          jobstop(job)
-          api.nvim_clear_autocmds({ group = kill_on_close })
-        end,
-      })
+      if lookup_table[watch_buffer] then
+        vim.notify("You're already watching this buffer.", vim.log.levels.WARN)
+      else
+        local kill_on_close =
+          augroup("markmap_kill_on_close", { clear = false })
+
+        job = jobstart(config.markmap_cmd, arguments, {
+          stderr_buffered = true, -- needed so on_stderr is only called once
+          on_stderr = function(_, data)
+            local message = table.concat(data, "\n")
+            message = message:gsub("\r", "")
+            vim.notify(message, vim.log.levels.ERROR)
+
+            lookup_table[watch_buffer] = nil
+            api.nvim_clear_autocmds({
+              group = kill_on_close,
+              buffer = watch_buffer,
+            })
+          end,
+        })
+
+        lookup_table[watch_buffer] = job
+
+        -- Register buffer local autocmd to kill job when buffer closes
+        autocmd("BufDelete", {
+          desc = "Kill markmap when watched buffer is closed",
+          buffer = watch_buffer,
+          group = kill_on_close,
+          callback = function()
+            jobstop(job)
+            lookup_table[watch_buffer] = nil
+            api.nvim_clear_autocmds({
+              group = kill_on_close,
+              buffer = watch_buffer,
+            })
+          end,
+        })
+      end
     end,
     { desc = "Show a mental map of the current file and watch for changes" }
   )
 
   cmd("MarkmapWatchStop", function()
-    if job ~= nil then jobstop(job) end -- kill jobs
+    local watch_buffer = vim.api.nvim_get_current_buf()
+    local job = lookup_table[watch_buffer]
+    if job then
+      jobstop(job)
+      lookup_table[watch_buffer] = nil
+      api.nvim_clear_autocmds({
+        group = kill_on_close,
+        buffer = watch_buffer,
+      })
+    end
   end, { desc = "Manually stops markmap watch" })
 end
 
